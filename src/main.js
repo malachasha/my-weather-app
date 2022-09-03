305512ace3f3c906e86ceb961fff9197681790c9
malachasha <68114464+malachasha@users.noreply.github.com>
2022-09-03 20:58:03 +0300

    added unit conversion

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 let apiKey = "54a3a2d6dda5f3e3dff81f647d318319";
+
+let cels = document.querySelector("#celsius");
+let fahr = document.querySelector("#fahrenheit");
+
 //the current date and time
 let dt = new Date();
 let day = document.querySelector("#day");
@@ -28,6 +32,7 @@ var months = [
   "November",
   "December",
 ];
+
 date.innerHTML =
   months[dt.getMonth()] + " " + dt.getDate() + ", " + dt.getFullYear();
 time.innerHTML =
@@ -36,7 +41,6 @@ time.innerHTML =
   ((dt.getMinutes() < 10 ? "0" : "").toString() + dt.getMinutes().toString()) +
   (dt.getHours() < 12 ? " AM" : " PM").toString();
 
-//display the city name on the page after the user submits the form.
 function ShowTemp(response) {
   let city = document.querySelector("#city");
   let windSpeed = document.querySelector(".windspeed");
@@ -44,6 +48,7 @@ function ShowTemp(response) {
   let pressure = document.querySelector(".pressure");
   let cityTemp = document.querySelector("#temperature");
   let weatherStatus = document.querySelector("#weather-status");
+  celsiusTemp = Math.round(response.data.main.temp);
   city.innerHTML = response.data.name;
   windSpeed.innerHTML = response.data.wind.speed + " Km/h";
   humidity.innerHTML = response.data.main.humidity + " %";
@@ -58,61 +63,51 @@ function displayCity() {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 
   axios.get(`${apiUrl}`).then(ShowTemp);
-}
-let btn = document.querySelector("button");
-//In your project, when a user searches for a city (example: New York),
-//it should display the name of the city on the result page
-//and the current temperature of the city.
-btn.addEventListener("click", displayCity);
-
-//Display a fake temperature (i.e 17) in Celsius and add a link to convert it to Fahrenheit.
-//When clicking on it, it should convert the temperature to Fahrenheit.
-//When clicking on Celsius, it should convert it back to Celsius.
-let cels = document.querySelector("#celsius");
-let fahr = document.querySelector("#fahrenheit");
-var temperature = document.querySelector("#temperature");
-
-function celsToFahr() {
-  temperature.innerHTML = "68";
-  cels.style.color = "#b0bec5";
-  fahr.style.color = "#212529";
-}
-function fahrToCels() {
-  temperature.innerHTML = "20";
   cels.style.color = "#212529";
   fahr.style.color = "#b0bec5";
 }
-cels.addEventListener("click", fahrToCels);
-fahr.addEventListener("click", celsToFahr);
+let btn = document.querySelector("button");
 
-//Add a Current Location button. When clicking on it,
+btn.addEventListener("click", displayCity);
+
+//When clicking on it, it should convert the temperature to Fahrenheit.
+//When clicking on Celsius, it should convert it back to Celsius.
+
+var temperature = document.querySelector("#temperature");
+
+function ShowFahr(event) {
+  event.preventDefault();
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  temperature.innerHTML = Math.round(fahrenheitTemp);
+  cels.style.color = "#b0bec5";
+  fahr.style.color = "#212529";
+}
+function ShowCels(event) {
+  event.preventDefault();
+  temperature.innerHTML = celsiusTemp;
+  cels.style.color = "#212529";
+  fahr.style.color = "#b0bec5";
+}
+
 //it uses the Geolocation API to get your GPS coordinates and display
 //and the city and current temperature using the OpenWeather API.
-function ShowCurrentTemp(response) {
-  let cityName = response.data.name;
-  let windSpeed = document.querySelector(".windspeed");
-  let humidity = document.querySelector(".humidity");
-  let weatherStatus = document.querySelector("#weather-status");
-  let pressure = document.querySelector(".pressure");
-  let cityTemp = document.querySelector("#temperature");
-  cityTemp.innerHTML = Math.round(response.data.main.temp);
-  let city = document.querySelector("#city");
-  city.innerHTML = cityName;
-  windSpeed.innerHTML = response.data.wind.speed + " Km/h";
-  humidity.innerHTML = response.data.main.humidity + " %";
-  pressure.innerHTML = response.data.main.pressure + " hPa";
-  weatherStatus.innerHTML = response.data.weather[0].description.toUpperCase();
-}
 
 function handlePosition(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(`${apiUrl}`).then(ShowCurrentTemp);
+  axios.get(`${apiUrl}`).then(ShowTemp);
 }
 
-function currentCity() {
+function currentCity(event) {
+  event.preventDefault();
   navigator.geolocation.getCurrentPosition(handlePosition);
+  cels.style.color = "#212529";
+  fahr.style.color = "#b0bec5";
 }
+
+let celsiusTemp = null;
 let locationBtn = document.querySelector(".current-btn");
 locationBtn.addEventListener("click", currentCity);
+cels.addEventListener("click", ShowCels);
+fahr.addEventListener("click", ShowFahr);
